2bd414b2d87a7a965f7270684be256cf4e08ef81
Yoan <yoan.principal@gmail.com>
2025-01-28 15:07:32 +0100

    Code corrigé mais modal ne s'ouvre pas

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import Modal from "../../containers/Modal"
+import Modal from "../../containers/Modal";
 
 import "./style.scss";
 
@@ -9,73 +9,95 @@ export const BUTTON_TYPES = {
   SUBMIT: 2,
 };
 
-const ModalButton = ({ title, onClick, type, disabled, children}) => {
-  const [setIsOpened] = useState(false);
-  
+const Button = ({ title, onClick, type, disabled, children }) => {
+  const [isOpened, setIsOpened] = useState(false);
+
   const handleClick = (e) => {
     onClick(e);
-    setIsOpened(true);
-   
-
-    
+    setIsOpened(true); // Ouvre la modale
   };
 
-  switch (type) {
-    case BUTTON_TYPES.DEFAULT:
-      return (
-        <button
-          type="button"
-          disabled={disabled}
-          className="Button"
-          data-testid="button-test-id"
-          onClick={handleClick}
-          title={title}
-        >
-          {children}
-        </button>
-      );
-    case BUTTON_TYPES.SUBMIT:
-      return (
-        <input
-          disabled={disabled}
-          className="Button"
-          type="submit"
-          data-testid="button-test-id"
-          value={children}
-          onClick={handleClick}
-          title={title}
-        />
-      );
-    default:
-      return (
-        <button
-          type="button"
-          disabled={disabled}
-          className="Button"
-          data-testid="button-test-id"
-          onClick={handleClick}
-          title={title}
-        >
-          {children}
-        </button>
-      );
-  }
+  return (
+    <>
+      {/* La modale sera affichée ici si `isOpened` est true */}
+      <Modal
+        opened={isOpened}
+        Content={
+          <div>
+            <p>Message envoyé !</p>
+            <button
+              type="button"
+              onClick={() => setIsOpened(false)} // Ferme la modale
+            >
+              Fermer
+            </button>
+          </div>
+        }
+      >
+        {() => {}}
+      </Modal>
+
+      {/* Bouton rendu via le switch */}
+      {(() => {
+        switch (type) {
+          case BUTTON_TYPES.DEFAULT:
+            return (
+              <button
+                type="button"
+                disabled={disabled}
+                className="Button"
+                data-testid="button-test-id"
+                onClick={handleClick}
+                title={title}
+              >
+                {children}
+              </button>
+            );
+          case BUTTON_TYPES.SUBMIT:
+            return (
+              <input
+                disabled={disabled}
+                className="Button"
+                type="submit"
+                data-testid="button-test-id"
+                value={children}
+                onClick={handleClick}
+                title={title}
+              />
+            );
+          default:
+            return (
+              <button
+                type="button"
+                disabled={disabled}
+                className="Button"
+                data-testid="button-test-id"
+                onClick={handleClick}
+                title={title}
+              >
+                {children}
+              </button>
+            );
+        }
+      })()}
+    </>
+  );
 };
 
-
-ModalButton.propTypes = {
+Button.propTypes = {
   title: PropTypes.string,
   onClick: PropTypes.func,
   type: PropTypes.number,
   disabled: PropTypes.bool,
   children: PropTypes.node,
 };
-ModalButton.defaultProps = {
+
+Button.defaultProps = {
   disabled: false,
   onClick: () => null,
   type: BUTTON_TYPES.DEFAULT,
   title: "",
-  children: null
-}
+  children: null,
+};
 
-export default ModalButton;
+export default Button;
